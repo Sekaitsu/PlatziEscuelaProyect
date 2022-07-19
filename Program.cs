61f385723a5e9215479f496858fd4762c43cf490
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-19 15:38:56 -0500

    Recorrido de un arreglo con un WHILE

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using Event1.Entidades;// se llaman los otros archivos desde el namespace a este archivo
 
 namespace Etapa1
@@ -12,41 +13,45 @@ namespace Etapa1
              * Se puede poner en el orden que uno desee se puede usar de ambos modos
              */
             var escuela = new clsEscuela("Platzi Academy", 2012, TiposEscuela.Primaria, pais: "Colombia", ciudad: "Bogotá");
-            /*
-             * se puede hacer de dos formas
-             * forma 1: 
-             * escuela.Nombre = "Platzi Academy";
-             * escuela.TipoEscuela = TiposEscuela.Primaria;
-            */
+
             var arregloCursos = new Curso[3];
+
             arregloCursos[0] = new Curso()
             {
                 Name = "101"
             };
-            //arregloCursos[1] = new Curso() { Name = "curso2" };
 
-            //arregloCursos[1] = Name = $"{curso2}";
+            var curso2 = new Curso()
+            {
+                Name = "201"
+            };
+            arregloCursos[1] = curso2;
 
 
+            var curso3 = new Curso()
+            {
+                Name = "301"
+            };
+            arregloCursos[2] = curso3;
 
 
-
-            var curso1 = new Curso() { Name = "101" };
-            var curso2 = new Curso() { Name = "201" };
-            var curso3 = new Curso() { Name = "301" };
-
-            //Console.WriteLine(escuela.Nombre);
-            //Console.WriteLine(escuela.YearCreacion);
-            //Console.WriteLine(escuela.Pais);
             Console.WriteLine(escuela);
-
-
             //IMPRIMIR RESULTADOS
             System.Console.WriteLine(" ================== ================== ");
-            Console.WriteLine(curso1.Name + "," + curso1.UniqueId); //forma 1 para agregar 
-            Console.WriteLine($"{curso2.Name} , {curso2.UniqueId}"); //forma 2 para agregar 
-            Console.WriteLine(curso3);
+            ImprimirCursos(arregloCursos);
         }
 
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int contador = 0;
+
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[contador].Name}, Id {arregloCursos[contador].UniqueId}");
+                contador++;
+            }
+
+
+        }
     }
 }
